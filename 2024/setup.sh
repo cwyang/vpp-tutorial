6c3faccd990b42d7c6ad315b76c7c44ef4d26408
Chul-Woong Yang <ycw@nhn.com>
2024-08-20 22:49:08 +0900

    erspan

--- 2024/setup.sh
+++ 2024/setup.sh
@@ -3,6 +3,8 @@
 vppctl='sudo vppctl'
 ip='sudo ip'
 
+tunnel_mode="erspan" # [ gre | erspan ], currently only erspan works
+
 function prepare_nic {
     local ns="$1"; shift
     local ipaddr="$1"; shift
@@ -21,18 +23,47 @@ function prepare_nic {
 
 }
 
+function setup_tunnel {
+    # https://developers.redhat.com/blog/2019/05/17/an-introduction-to-linux-virtual-interfaces-tunnels#erspan_and_ip6erspan
+    local type="${1:-gre}"; shift
+    local nic="${type}1"
+
+    if [ "$type" = "erspan" ]; then
+	$ip netns exec span ip link add dev $nic type $type remote 10.20.1.2 local 10.20.1.1 seq key 1 erspan_ver 1 erspan 1
+    else
+	$ip netns exec span ip tunnel add $nic mode $type remote 10.20.1.2 local 10.20.1.1
+	#$ip netns exec span ip link add dev $nic type $type remote 10.20.1.2 local 10.20.1.1
+    fi
+    $ip netns exec span ip addr add 192.168.100.1/32 dev $nic
+    $ip netns exec span ip link set $nic up
+}
+
 function setup_host {
     prepare_nic svr 10.10.1.1/24 10.10.1.255 10.10.1.2
     prepare_nic span 10.20.1.1/24 10.20.1.255 10.20.1.2
-    # gre 잘안돼
-    #$ip netns exec span ip tunnel add gre1 mode gre remote 10.20.1.2 local 10.20.1.1
-    #$ip netns exec span ip addr add 192.168.100.1/32 dev gre1
-    #$ip netns exec span ip link set gre1 up
-    # erspan 잘안돼
-    # https://developers.redhat.com/blog/2019/05/17/an-introduction-to-linux-virtual-interfaces-tunnels#erspan_and_ip6erspan
-    #$ip netns exec span ip link add dev erspan1 type erspan local 10.20.1.1 remote 10.20.1.2 seq key 1 erspan_ver 1 erspan 1
-    #$ip netns exec span ip addr add 192.168.100.1/32 dev erspan1
-    #$ip netns exec span ip link set erspan1[ up
+    
+    # gre / erspan
+    setup_tunnel $tunnel_mode
+}
+
+function setup_vpp_tunnel {
+    local type="${1:-gre}"; shift
+    local arg=""
+    if [ "$type" = "erspan" ]; then
+	arg="erspan 1"
+    fi
+
+    # erspan 없이 gre모드에서 span하면 
+    #span vrf를 별도로 분리하면 어떤 때 좋은가?
+    #$vppctl ip table add 1
+
+    #$vppctl create gre tunnel src 10.20.1.2 dst 10.20.1.1 instance 1 outer-table-id 0 $arg
+    $vppctl create gre tunnel src 10.20.1.2 dst 10.20.1.1 instance 1 outer-table-id 0 $arg
+    $vppctl set inter ip addr gre1 192.168.100.2/32
+    $vppctl set inter state gre1 up
+    $vppctl set inter span host-svr-vpp destination gre1
+    #$vppctl show gre tunnel
+    #$vppctl show inter span
 }
 
 function setup_vpp {
@@ -52,16 +83,31 @@ function setup_vpp {
     $vppctl set interface nat44 in host-svr-vpp out GigabitEthernet0/4/0
     $vppctl nat44 add static mapping tcp local 10.10.1.1 8000 external 192.168.0.50 8000  # 10.10.1.1!
 
-    #$vppctl ip table add 1
-    #뒤에 erspan 1을 떼면 통신이 안됨. erspan의 의미?
-    #gre span과 erspan의 차이
-    #$vppctl create gre tunnel src 10.20.1.2 dst 10.20.1.1 instance 1 outer-table-id 0 erspan 1
-    #$vppctl set inter ip addr gre1 192.168.100.2/32
-    #$vppctl set inter state gre1 up
-    #$vppctl set inter span host-svr-vpp destination gre1
-    #$vppctl show gre tunnel
-    #$vppctl show inter span
+    # gre / erspan
+    setup_vpp_tunnel $tunnel_mode
 }
+ 
 
 setup_host
 setup_vpp
+
+exit 0
+
+## gre span시 erspan arg 없으면 죽는 곳
+Aug 20 22:19:14 mars vnet[27897]: /home/ubuntu/vpp-dev/src/vnet/adj/adj.h:462 (adj_get) assertion `! pool_is_free (adj_pool, _e)' fails
+Aug 20 22:19:14 mars vnet[27897]: received signal SIGABRT, PC 0x7f4fa21ab9fc
+Aug 20 22:19:14 mars vnet[27897]: #0  0x00007f4fa25e5e5c unix_signal_handler + 0x1ec
+Aug 20 22:19:14 mars vnet[27897]: #1  0x00007f4fa2157520 0x7f4fa2157520
+Aug 20 22:19:14 mars vnet[27897]: #2  0x00007f4fa21ab9fc pthread_kill + 0x12c
+Aug 20 22:19:14 mars vnet[27897]: #3  0x00007f4fa2157476 raise + 0x16
+Aug 20 22:19:14 mars vnet[27897]: #4  0x00007f4fa213d7f3 abort + 0xd3
+Aug 20 22:19:14 mars vnet[27897]: #5  0x000055a3bd2840a3 0x55a3bd2840a3
+Aug 20 22:19:14 mars vnet[27897]: #6  0x00007f4fa24545c9 debugger + 0x9
+Aug 20 22:19:14 mars vnet[27897]: #7  0x00007f4fa2454380 _clib_error + 0x210
+Aug 20 22:19:14 mars vnet[27897]: #8  0x00007f4fa2fd1cee adj_get + 0x8e
+Aug 20 22:19:14 mars vnet[27897]: #9  0x00007f4fa2fd175f adj_l2_rewrite_inline + 0x14f
+Aug 20 22:19:14 mars vnet[27897]: #10 0x00007f4fa2fd1ade adj_l2_midchain_node_fn_hsw + 0x5e
+Aug 20 22:19:14 mars vnet[27897]: #11 0x00007f4fa258aaa2 dispatch_node + 0x322
+Aug 20 22:19:14 mars vnet[27897]: #12 0x00007f4fa258b4c2 dispatch_pending_node + 0x3b2
+Aug 20 22:19:14 mars vnet[27897]: #13 0x00007f4fa25869e8 vlib_main_or_worker_loop + 0x898
+Aug 20 22:19:14 mars vnet[27897]: #14 0x00007f4fa258822a vlib_main_loop + 0x1a
